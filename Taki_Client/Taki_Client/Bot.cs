9722809227f0cdbf3cf11293e408b2a5197d7c0c
Tomer Sarel <tom.sarel@gmail.com>
2020-10-14 19:23:41 +0300

    Started to implement a rule based mechanism for the bot

--- Taki_Client/Taki_Client/Bot.cs
+++ Taki_Client/Taki_Client/Bot.cs
@@ -6,17 +6,30 @@ using System.Threading.Tasks;
 
 namespace Taki_Client
 {
+    public enum StateCodes { NextNeighborLowCards, LastNeighborLowCards}
     class Bot
     {
         private Card topCard;
         private Deck deck;
         private List<Enemy> enemies;
+        private List<Rule> rules;
+        private int numOfPlayers;
+        private const int LOW_CARDS = 3;
 
         public Bot(Card topCard, Deck deck, List<Enemy> enemies)
         {
             this.topCard = topCard;
             this.deck = deck;
             this.enemies = enemies;
+            this.rules = new List<Rule>();
+        }
+
+        private void GenerateRules()
+        {
+            //Example for a how a rule might be
+            Rule rule = new Rule(StateCodes.NextNeighborLowCards.ToString(), 
+                new ValidTypes[] { ValidTypes.plus_2, ValidTypes.stop, ValidTypes.change_direction});
+            this.rules.Add(rule);
         }
 
         public void UpdateTopCard(Card card)
@@ -28,7 +41,32 @@ namespace Taki_Client
         {
             //Returns the list of cards to be played
             //Returns null when the chosen action is drawing cards
+            string currentState = GenerateState();
+            ValidTypes[] priorityList;
+            foreach(Rule rule in this.rules)
+            {
+                if (rule.Match(currentState))
+                {
+                    priorityList = rule.PriorityList;
+                    break;
+                }
+            }
+            //TODO: Check the priority list against the actual deck and determine the optimal action
             return null;
+        }
+
+        private string GenerateState()
+        {
+            //Returns string that contains all the relevant flags for the current state
+            string state = "";
+            foreach(Enemy enemy in this.enemies)
+            {
+                if (enemy.NumOfCards < LOW_CARDS && enemy.Position == 1)
+                    state += StateCodes.NextNeighborLowCards + " ";
+                if (enemy.NumOfCards < LOW_CARDS && enemy.Position == this.numOfPlayers - 1)
+                    state += StateCodes.LastNeighborLowCards + " ";
+            }
+            return state;
         }
     }
 }
--- Taki_Client/Taki_Client/Enemy.cs
+++ Taki_Client/Taki_Client/Enemy.cs
@@ -9,17 +9,32 @@ namespace Taki_Client
     class Enemy
     {
         private int numOfCards;
+        private int relativePosition; // the number of the player according to the playing order if our bot is number 0
+
         public int NumOfCards { get { return numOfCards; } }
+        public int Position { get { return this.relativePosition; } }
         private const int INITIAL_NUM_OF_CARDS = 8;
 
-        public Enemy()
+        public Enemy(int relativePosition)
         {
             this.numOfCards = INITIAL_NUM_OF_CARDS;
+            this.relativePosition = relativePosition;
         }
 
         public void UpdateState(int numOfCardsPlayed)
         {
-            this.numOfCards += numOfCardsPlayed;
+            /*
+             * numOfCardsPlayed - positive if the enemy got rid of cards, negative if he drew cards
+             */
+            this.numOfCards -= numOfCardsPlayed;
+        }
+
+        public void FlipOrder(int numOfPlayers)
+        {
+            /*
+             * To be executed when change direction card is played
+             */ 
+            this.relativePosition = numOfPlayers - this.relativePosition;
         }
     }
 }
