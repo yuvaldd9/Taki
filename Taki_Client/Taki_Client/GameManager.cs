8e773d4cdbe80042abe6c5e712e55eb25f61dfac
Tomer Sarel <tom.sarel@gmail.com>
2020-11-14 14:39:13 +0200

    Created a game manager which handles the game and integrate communication, graphics and bot

---
+++ Taki_Client/Taki_Client/GameManager.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Net.Sockets;
+using System.Windows.Forms;
+namespace Taki_Client
+{
+    class GameManager
+    {
+        private Bot bot;
+        private GamePanel panel;
+        private List<Enemy> enemies;
+        private Deck deck;
+        private string playerName;
+        private Card topCard;
+        private Socket sock;
+        private string jwt;
+        public GameManager(Deck deck, List<Enemy> enemies, string playerName, Card topCard, Socket sock, string jwt, Control parent, GamePanel panel)
+        {
+            this.jwt = jwt;
+            this.deck = deck;
+            this.enemies = enemies;
+            this.playerName = playerName;
+            this.topCard = topCard;
+            this.bot = new Bot(topCard, deck, enemies);
+            this.panel = panel;
+            this.panel.Initialize(topCard);
+            this.sock = sock;
+        }
+
+        public void Run()
+        {
+            string[] jsonArr;
+            dynamic args;
+            string type, playerName;
+            int amount;
+            List<Card> action, taken = null;
+            JArray cards;
+            dynamic jCards;
+            bool waitingForCards = false;
+            
+            string[] data = Communication.GameHandler(sock, "NOT MY TURN", null);
+            while (true)
+            {
+                foreach (string json in data)
+                {
+                    Console.WriteLine(json);
+                    jsonArr = Communication.JsonAnalyzer(json);
+                    args = JsonConvert.DeserializeObject(jsonArr[1]);
+                    switch (jsonArr[0])
+                    {
+                        case "update_turn":
+
+                            playerName = args.current_player;
+
+                            if (playerName == this.playerName)
+                            {
+                                action = bot.ChooseAction();
+                                if (action != null && action.Count > 0)
+                                {
+                                    
+
+                                    Communication.SendMsg(this.sock, "place_cards", new object[] { action.ToArray(), this.jwt });
+                                    foreach (Card card in action)
+                                        this.bot.deck.RemoveCard(card);
+                                }
+                                else
+                                {
+                                    Communication.SendMsg(this.sock, "take_cards", new string[] { this.jwt });
+                                    waitingForCards = true;
+                                }
+
+                            }
+
+                            panel.UpdateCurrentPlayer((string)playerName);
+                            break;
+                        case "move_done":
+                            type = (string)args.type;
+                            playerName = (string)args.player_name;
+                            if (type == "cards_taken")
+                            {
+                                if (playerName == this.playerName && waitingForCards)
+                                {
+                                }
+                                else
+                                {
+                                    amount = int.Parse((string)args.amount);
+                                    List<Card> cards_taken = new List<Card>();
+                                    for (int i = 0; i < amount; i++)
+                                        cards_taken.Add(null);
+                                    panel.TakeCards(playerName, cards_taken);
+                                    System.Threading.Thread.Sleep(7000);
+                                    foreach(Enemy enemy in this.enemies)
+                                    {
+                                        if (enemy.name == playerName)
+                                            enemy.UpdateState(-amount);
+                                    }
+                                }
+                                this.bot.isTopCardActive = false;
+                            }
+                            else
+                            {
+
+                                cards = args.cards;
+
+                                action = new List<Card>();
+                                foreach (object card in cards)
+                                {
+                                    jCards = JsonConvert.DeserializeObject(card.ToString());
+                                    Card c = new Card((string)jCards.type, (string)jCards.color, (string)jCards.value);
+                                    action.Add(c);
+                                }
+                                panel.PlayCards(playerName, action);
+                                System.Threading.Thread.Sleep(7000);
+                                bot.UpdateTopCard(new Card(action.ElementAt(action.Count - 1)));
+                                this.bot.isTopCardActive = true;
+                                foreach (Enemy enemy in this.enemies)
+                                {
+                                    if (enemy.name == playerName)
+                                        enemy.UpdateState(action.Count);
+                                }
+                            }
+                            break;
+                        case "success":
+                            if(waitingForCards && args.cards != null)
+                            {
+                                cards = args.cards;
+
+                                taken = new List<Card>();
+                                foreach (object card in cards)
+                                {
+                                    jCards = JsonConvert.DeserializeObject(card.ToString());
+                                    Card c = new Card((string)jCards.type, (string)jCards.color, (string)jCards.value);
+                                    taken.Add(c);
+                                    this.deck.AddCard(c);
+                                }
+                                panel.TakeCards(this.playerName, taken);
+                                taken = null;
+                                System.Threading.Thread.Sleep(7000);
+                                waitingForCards = false;
+                            }
+                            break;
+                        case "player_finished":
+                            break;
+                        case "game_ended":
+                            break;
+                        default:
+                            Console.WriteLine(jsonArr[0]);
+                            Console.WriteLine(jsonArr[1]);
+                            break;
+                    }
+
+                }
+                data = Communication.GameHandler(sock, "NOT MY TURN", null);
+
+            }
+            
+        }
+    }
+}
