f82d36ce8cb80b97e77196913cbc822b8ff97074
Tomer Sarel <tom.sarel@gmail.com>
2020-11-18 14:00:17 +0200

    Added documentation and refactored a recurring bit of code into a method

--- Taki_Client/Taki_Client/LeaderboardPanel.cs
+++ Taki_Client/Taki_Client/LeaderboardPanel.cs
@@ -10,7 +10,7 @@ namespace Taki_Client
 {
     class LeaderboardPanel : Panel
     {
-        private string[] leaderboard;
+        private string[] leaderboard; //The names of the players, ordered by the time they finished the game
         public LeaderboardPanel(string[] leaderboard)
         {
             this.leaderboard = leaderboard;
@@ -20,64 +20,63 @@ namespace Taki_Client
 
         public void Initialize()
         {
+            //Add a title
             Label title = new Label();
             title.Text = "Leaderboard";
             title.Font = new Font("Arial", 20);
             title.AutoSize = true;
-            this.Invoke(new MethodInvoker(delegate () { this.Controls.Add(title); }));
-            this.Invoke(new MethodInvoker(delegate () { title.Location = new Point((this.Width - title.Width) / 2, 30); }));
+            this.PlaceControl(title, new Point((this.Width - title.Width) / 2, 30));
 
+            //Present the players' names with their rank and decorate them with a color according to their rank
             Label firstPlace = new Label();
             firstPlace.Text = "First: " + this.leaderboard[0];
             firstPlace.Font = new Font("Arial", 14);
             firstPlace.AutoSize = true;
             firstPlace.BackColor = Color.Gold;
-            this.Invoke(new MethodInvoker(delegate () { this.Controls.Add(firstPlace); }));
-            this.Invoke(new MethodInvoker(delegate () { firstPlace.Location = new Point((this.Width - firstPlace.Width) / 2, 200); }));
-
+            this.PlaceControl(firstPlace, new Point((this.Width - firstPlace.Width) / 2, 200));
 
             Label secondPlace = new Label();
             secondPlace.Text = "Second: " + this.leaderboard[1];
             secondPlace.Font = new Font("Arial", 14);
             secondPlace.AutoSize = true;
             secondPlace.BackColor = Color.Silver;
-            this.Invoke(new MethodInvoker(delegate () { this.Controls.Add(secondPlace); }));
-            this.Invoke(new MethodInvoker(delegate () { secondPlace.Location = new Point((this.Width - firstPlace.Width) / 2, 300); }));
-
+            this.PlaceControl(secondPlace, new Point((this.Width - firstPlace.Width) / 2, 300));
 
             Label thirdPlace = new Label();
             thirdPlace.Text = "Third: " + this.leaderboard[2];
             thirdPlace.Font = new Font("Arial", 14);
             thirdPlace.AutoSize = true;
             thirdPlace.BackColor = Color.Chocolate;
-            this.Invoke(new MethodInvoker(delegate () { this.Controls.Add(thirdPlace); }));
-            this.Invoke(new MethodInvoker(delegate () { thirdPlace.Location = new Point((this.Width - firstPlace.Width) / 2, 400); }));
-
-
+            this.PlaceControl(thirdPlace, new Point((this.Width - firstPlace.Width) / 2, 400));
+            
             Label forthPlace = new Label();
             forthPlace.Text = "Forth: " + this.leaderboard[3];
             forthPlace.Font = new Font("Arial", 14);
             forthPlace.AutoSize = true;
             forthPlace.BackColor = Color.Transparent;
-            this.Invoke(new MethodInvoker(delegate () { this.Controls.Add(forthPlace); }));
-            this.Invoke(new MethodInvoker(delegate () { forthPlace.Location = new Point((this.Width - firstPlace.Width) / 2, 500); }));
+            this.PlaceControl(forthPlace, new Point((this.Width - firstPlace.Width) / 2, 500));
 
-
+            //Add a button to return to the home window
             Button homeButton = new Button();
             homeButton.Text = "Main Menu";
             homeButton.Font = new Font("Arial", 12);
             homeButton.AutoSize = true;
             homeButton.BackColor = Color.Green;
             homeButton.Click += new EventHandler(this.homeButton_Click);
-            this.Invoke(new MethodInvoker(delegate () { this.Controls.Add(homeButton); }));
-            this.Invoke(new MethodInvoker(delegate () { homeButton.Location = new Point((this.Width - firstPlace.Width) / 2, this.Height - homeButton.Height - 40); }));
+            this.PlaceControl(homeButton, new Point((this.Width - firstPlace.Width) / 2, this.Height - homeButton.Height - 40));
+           
+        }
 
-
-
+        private void PlaceControl(Control control, Point location)
+        {
+            //Place a control on the panel
+            this.Invoke(new MethodInvoker(delegate () { this.Controls.Add(control); }));
+            this.Invoke(new MethodInvoker(delegate () { control.Location = location; }));
         }
 
         private void homeButton_Click(object sender, EventArgs e)
         {
+            //Remove the cuurent panel and load the home panel
             HomePanel panel = new HomePanel();
             this.Parent.Invoke(new MethodInvoker(delegate () { this.Parent.Controls.Add(panel); }));
             this.Parent.Invoke(new MethodInvoker(delegate () { this.Parent.Controls.Remove(this); }));
